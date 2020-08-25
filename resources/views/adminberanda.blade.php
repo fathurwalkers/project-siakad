65b958cee2d8fa419c0afa825617043f703d78a6
fathurwalkers <fathurwalkers44@gmail.com>
2020-08-25 17:01:14 +0800

    templating halaman admin dan menambahkan db post

--- resources/views/adminberanda.blade.php
+++ resources/views/adminberanda.blade.php
@@ -1,1 +1,28 @@
 @extends('layouts.adminlayout')
+
+{{-- custom css jika ada --}}
+@push('css-tambahan')
+{{-- tulis kode disini  --}}
+@endpush
+{{-- end - custom css jika ada --}}
+
+
+{{-- membuat menu sidebar disini  --}}
+@section('sidebar-menu')
+{{-- tulis kode disini  --}}
+@endsection
+{{-- end - membuat menu sidebar disini  --}}
+
+
+{{-- Isi dari main content  --}}
+@section('main-content')
+{{-- tulis kode disini  --}}
+@endsection
+{{-- end - Isi dari main content  --}}
+
+
+{{-- custom javascript jika ada --}}
+@push('js-tambahan')
+{{-- tulis kode disini  --}}
+@endpush
+{{-- end - custom javascript jika ada --}}
